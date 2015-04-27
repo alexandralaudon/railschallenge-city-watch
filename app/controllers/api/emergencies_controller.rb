0d6a62c3d20288dda658c15790b40e074cb8aa60
Alex <alexandralaudon@gmail.com>
2015-04-27 14:00:45 -0600

    created create method in rails

--- app/controllers/api/emergencies_controller.rb
+++ app/controllers/api/emergencies_controller.rb
@@ -1,6 +1,12 @@
 module Api
   class EmergenciesController < Api::BaseController
 
+    def create
+      emergency_params = params.require(:emergency).permit(:code, :fire_severity, :police_severity, :medical_severity, :resolved_at)
+      @emergency = Emergency.create!(emergency_params)
+      render json: @emergency, status: :created
+      # ex status: unprocessable_entity => 422
+    end
     private
 
       def emergencies_params
